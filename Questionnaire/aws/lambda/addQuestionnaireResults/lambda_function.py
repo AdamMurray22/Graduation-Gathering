86a1dc6a6f18a9c8b57c1993711f33b8739d1515
Adam-Murray <adammurray1122@gmail.com>
2024-01-20 00:12:21 +0000

    Updated questionnaire results lambda function

--- Questionnaire/aws/lambda/addQuestionnaireResults/lambda_function.py
+++ Questionnaire/aws/lambda/addQuestionnaireResults/lambda_function.py
@@ -8,19 +8,34 @@ logger.setLevel(logging.INFO)
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    return getGeoJsonFromS3Bucket()
-
-def getGeoJsonFromS3Bucket():
     s3_Bucket_Name = "graduation-gathering-questionnaire-results"
-    s3_File_Name = "Questionnaire Results.txt"
+    s3_File_Name = "Questionnaire Results.json"
+
+    bucketContent = getJsonFromS3Bucket(s3_Bucket_Name, s3_File_Name)
+
+    serveyResponse = event["body"]
+
+    writeJsonToS3Bucket(s3_Bucket_Name, s3_File_Name, bucketContent, serveyResponse)
+
+def getJsonFromS3Bucket(s3_Bucket_Name, s3_File_Name):
     
     try:
         object = s3_client.get_object(Bucket=s3_Bucket_Name, Key=s3_File_Name)
         body = object['Body']
         jsonString = body.read().decode('utf-8')
-        routeGeoJsons = []
-        routeGeoJsons.append(json.loads(jsonString))
-        return routeGeoJsons
+        return json.loads(jsonString)
     except:
         logger.error("Could not find file: " + s3_Bucket_Name + "/" + s3_File_Name)
     return []
+
+def writeJsonToS3Bucket(s3_Bucket_Name, s3_File_Name, bucketContent, serveyResponse):
+    serveyResponse = serveyResponse.replace("'", "")
+    serveyResponse = serveyResponse.replace("'", "")
+
+    bucketContent["Responses"].append(serveyResponse)
+    bucketContentAsString = str(bucketContent)
+    bucketContentAsString = bucketContentAsString.replace("'", '"')
+    encoded_string = bucketContentAsString.encode("utf-8")
+
+    s3 = boto3.resource("s3")
+    s3.Bucket(s3_Bucket_Name).put_object(Key=s3_File_Name, Body=encoded_string)
