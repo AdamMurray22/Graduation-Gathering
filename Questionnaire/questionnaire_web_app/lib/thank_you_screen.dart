e4cbea8afd727ee3a8de8d63c21f64e8f4e6ebf3
Adam-Murray <adammurray1122@gmail.com>
2024-01-21 02:33:33 +0000

    Completed Thank you screen and thus completed questionnaire

--- Questionnaire/questionnaire_web_app/lib/thank_you_screen.dart
+++ Questionnaire/questionnaire_web_app/lib/thank_you_screen.dart
@@ -11,6 +11,18 @@ class _ThankYouScreenState extends State<ThankYouScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Text("Thank you for taking part in this survey"));
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+      Text(
+        "Answers Submitted!",
+        style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+      Text("Thank you for taking part in this survey!",
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center)
+    ])));
   }
 }
