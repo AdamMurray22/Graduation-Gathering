ef3c157b83ce816ba8ee9b661829091c6ee337d3
Adam-Murray <adammurray1122@gmail.com>
2024-01-30 19:30:44 +0000

    Adds an extra question and changes q3 to a dropdown box

--- Questionnaire/questionnaire_web_app/lib/questions_screen.dart
+++ Questionnaire/questionnaire_web_app/lib/questions_screen.dart
@@ -20,21 +20,45 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   final List<String> _q2ReorderableList = [
     "Users of the app are verified as University of Portsmouth staff or graduates",
     "That you can be selective about which graduation zones you can be seen in by others",
-    "That you can be selective about which graduation zones you can see other in",
+    "That you can be selective about which graduation zones you can see others in",
     "You can filter which users you see by course",
     "You can filter which users you see by name",
     "You can filter which users you see by whether you've already met",
     "A messaging service to allow you to communicate with others",
   ];
 
-  final List<String> _q3ReorderableList = [
+  final List<DropdownMenuItem<Text>> _q3DropDownItems = [
+    const DropdownMenuItem(value: Text("I want this as a zone"), child: Text("I want this as a zone")),
+    const DropdownMenuItem(value: Text("I don't care if this is a zone"), child: Text("I don't care if this is a zone")),
+    const DropdownMenuItem(value: Text("I don't want this as a zone"), child: Text("I don't want this as a zone")),
+  ];
+
+  final List<DropdownButton> _q3Buttons = [];
+
+  final List<Text> _q3ButtonsValues = [];
+
+  final List<String> _q3List = [
     "Guildhall square",
     "Ravelin park",
     "The area between Guildhall and Ravelin park",
     "Pryzm"
   ];
 
+  final Map<String, String> _q3Answers = {};
+
   String _q4Text = "";
+
+  String _q5Text = "";
+
+  @override
+  void initState() {
+    for (int i = 0; i < _q3List.length; i++)
+    {
+      _q3Answers[_q3List[i]] =  "I want this as a zone";
+      _q3ButtonsValues.add(_q3DropDownItems[0].value!);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,6 +116,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
                           ])),
                       const SizedBox(height: 7),
                       const Text(
+                          "A Graduation zone in this context means an area of portsmouth that can be designated on the app to show peoples locations.", style: TextStyle(fontSize: 17)),
+                      const SizedBox(height: 7),
+                      const Text(
                           "Q2. Please arrange the following list of features from most important to you at the top, to least important to you at the bottom:", style: TextStyle(fontSize: 17)),
                       const SizedBox(height: 7),
                       Padding(
@@ -108,23 +135,59 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
                               .toList())),
                       const SizedBox(height: 14),
                       const Text(
-                          "A Graduation zone in this context means an area of portsmouth that can be designated on the app to show peoples locations.", style: TextStyle(fontSize: 17)),
-                      const SizedBox(height: 7),
-                      const Text(
                           "Q3. Please arrange the following list of graduation zones from most important to you at the top, to least important to you at the bottom:", style: TextStyle(fontSize: 17)),
                       const SizedBox(height: 7),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                        child:
-                      ReorderableListView(
-                          scrollController: ScrollController(),
-                          shrinkWrap: true,
-                          onReorder: (oldIndex, newIndex) {
-                            _onReorder(_q3ReorderableList, oldIndex, newIndex);
-                          },
-                          children: _q3ReorderableList
-                              .map((item) => Text(item, key: Key(item), style: const TextStyle(fontSize: 17)))
-                              .toList())),
+                      // Padding(
+                      //   padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                      //   child:
+                      // ReorderableListView(
+                      //     scrollController: ScrollController(),
+                      //     shrinkWrap: true,
+                      //     onReorder: (oldIndex, newIndex) {
+                      //       _onReorder(_q3ReorderableList, oldIndex, newIndex);
+                      //     },
+                      //     children: _q3ReorderableList
+                      //         .map((item) => Text(item, key: Key(item), style: const TextStyle(fontSize: 17)))
+                      //         .toList())),
+                      Row(children: [
+                        Text(_q3List[0]),
+                        DropdownButton(
+                            value: _q3ButtonsValues[0],
+                            items: _q3DropDownItems,
+                            onChanged: (text) {
+                              _q3Answers[_q3List[0]] = text!.data!;
+                              _q3ButtonsValues[0] = text;
+                              setState(() {
+
+                              });
+                            })
+                      ]),
+                      Row(children: [
+                        Text(_q3List[1]),
+                        DropdownButton(
+                            value: _q3ButtonsValues[1],
+                            items: _q3DropDownItems,
+                            onChanged: (text) {
+                              _q3Answers[_q3List[1]] = text!.data!;
+                              _q3ButtonsValues[1] = text;
+                              setState(() {
+
+                              });
+                            })
+                      ]),
+                      Row(children: [
+                        Text(_q3List[2]),
+                        DropdownButton(
+                            value: _q3ButtonsValues[2],
+                            items: _q3DropDownItems,
+                            onChanged: (text) {
+                              _q3Answers[_q3List[2]] = text!.data!;
+                              _q3ButtonsValues[2] = text;
+                              setState(() {
+
+                              });
+                            })
+                      ]),
                       const SizedBox(height: 7),
                       const Text(
                           "Q4(Optional). Please enter any other parts of Portsmouth you would like to be designated as graduation zones:", style: TextStyle(fontSize: 17)),
@@ -137,6 +200,22 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
                           border: OutlineInputBorder(
                             borderSide:
                                 BorderSide(color: Colors.black, width: 0.0),
+                          ),
+                          hintText: 'Enter text here...',
+                        ),
+                      ),
+                      const SizedBox(height: 7),
+                      const Text(
+                          "Q5(Optional). Please enter any other features you would like to see in this app:", style: TextStyle(fontSize: 17)),
+                      const SizedBox(height: 7),
+                      TextField(
+                        onChanged: (text) {
+                          _q5Text = text;
+                        },
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide:
+                            BorderSide(color: Colors.black, width: 0.0),
                           ),
                           hintText: 'Enter text here...',
                         ),
@@ -200,7 +279,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
   bool _submitResults() {
     SurveyAnswers answers = SurveyAnswers(
-        _q1CheckBoxes, _q2ReorderableList, _q3ReorderableList, _q4Text);
+        _q1CheckBoxes, _q2ReorderableList, _q3Answers, _q4Text, _q5Text);
     if (!answers.answersValid()) {
       return false;
     }
