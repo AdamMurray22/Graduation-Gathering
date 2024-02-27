dbc280753318bbb4c4190df87223495ff6205372
Adam-Murray <adammurray1122@gmail.com>
2024-02-27 01:21:42 +0000

    Updated setLocation lambda to check if the users location is within the users graduation zones

--- Graduation-Gathering/aws/lambda/setLocation/lambda_function.py
+++ Graduation-Gathering/aws/lambda/setLocation/lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 import logging
 import re
 import setLocation.package.pymysql as pymysql
+from setLocation.package.shapely.geometry import shape, GeometryCollection, Point
 import os
 import sys
 import time
@@ -33,7 +34,13 @@ def lambda_handler(event, context):
     messageBody = json.loads(messageBodyJson)
     location = messageBody["location"]
 
-    sql_string = 'update user set latitude = {latitude}, longitude = {longitude}, location_set = {locationSet} where user_id = "{userID}"'
+    userZones = getUserZonesGeoJsons(userID)
+
+    # If location not within one of the users zones then return without adding the user location to the database
+    if not userLocationInZone(location, userZones):
+        return
+
+    sql_string = 'UPDATE user SET latitude = {latitude}, longitude = {longitude}, location_set = {locationSet} WHERE user_id = "{userID}"'
 
     with conn.cursor() as cur:
         try:
@@ -42,6 +49,32 @@ def lambda_handler(event, context):
             logger.error(e)
         cur.close()
     conn.commit()
+
+def getUserZonesGeoJsons(userID):
+    get_user_zones_sql_string = 'SELECT graduation_zones.zone_name, graduation_zones_text.geojson FROM user_zones JOIN graduation_zones ON user_zones.zone_id = graduation_zones.zone_id JOIN graduation_zones_text ON graduation_zones.zone_id = graduation_zones_text.zone_id WHERE user_zones.user_id = "{userID}"'
+    with conn.cursor() as cur:
+        try:
+            cur.execute(get_user_zones_sql_string.format(userID = escape_sql_string(userID)))
+            zones = []
+            for row in cur:
+                zones.append({"Zone Name": row[0], "GeoJson": row[1]})
+        except pymysql.MySQLError as e:
+            logger.error(e)
+        cur.close()
+    conn.commit()
+    return zones
+
+def userLocationInZone(location, zones):
+    point = Point(location['long'], location['lat'])
+
+    for zone in zones:
+
+        polygon = shape(zone['GeoJson']['geometry'])
+
+        if polygon.contains(point):
+            return True
+    # If location is not within any of the users selected zones then return false
+    return False
     
 def escape_sql_string(sql_string):
     translate_table = str.maketrans({"]": r"\]", "\\": r"\\",
