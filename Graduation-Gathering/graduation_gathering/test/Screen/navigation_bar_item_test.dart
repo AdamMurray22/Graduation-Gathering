d3ded6bf83a2eca9ec14eda96b89da15d2ecb8f1
Adam-Murray <adammurray1122@gmail.com>
2024-03-10 02:45:06 +0000

    Fixed tests

--- Graduation-Gathering/graduation_gathering/test/Screen/navigation_bar_item_test.dart
+++ Graduation-Gathering/graduation_gathering/test/Screen/navigation_bar_item_test.dart
@@ -1,4 +1,6 @@
 import 'package:graduation_gathering/Auth/auth_token.dart';
+import 'package:graduation_gathering/Profile/academic_structure.dart';
+import 'package:graduation_gathering/Profile/profile_settings.dart';
 import 'package:graduation_gathering/Screen/navigation_bar_items.dart';
 import 'package:test/test.dart';
 
@@ -6,30 +8,34 @@ void main() {
   group('Navigation Bar Item Tests', () {
 
     AuthToken authToken = AuthToken("");
+    ProfileSettings profile = ProfileSettings(true, "", "", "", null, null, null, null);
+    AcademicStructure structure = AcademicStructure({});
 
     test('.getSelectedIndex() default value', () {
-      NavigationBarItems navigationBarItems = NavigationBarItems(authToken);
+      NavigationBarItems navigationBarItems = NavigationBarItems(authToken, profile, structure);
       expect(navigationBarItems.getSelectedIndex(), 0);
     });
 
     test('.getSelectedIndex() value changed', () {
-      NavigationBarItems navigationBarItems = NavigationBarItems(authToken);
+      NavigationBarItems navigationBarItems = NavigationBarItems(authToken, profile, structure);
       navigationBarItems.setSelectedIndex(3);
       expect(navigationBarItems.getSelectedIndex(), 3);
     });
 
     test('.getValuesInOrder() default nav bar items', () {
-      NavigationBarItems navigationBarItems = NavigationBarItems(authToken);
+      NavigationBarItems navigationBarItems = NavigationBarItems(authToken, profile, structure);
       expect(navigationBarItems.getValuesInOrder(), [
         navigationBarItems.mapScreen,
+        navigationBarItems.profileScreen,
         navigationBarItems.aboutScreen
       ]);
     });
 
     test('.getValuesInOrder() default nav bar items', () {
-      NavigationBarItems navigationBarItems = NavigationBarItems(authToken);
+      NavigationBarItems navigationBarItems = NavigationBarItems(authToken, profile, structure);
       expect(navigationBarItems.getScreensInOrder(), [
         navigationBarItems.mapScreen.item2,
+        navigationBarItems.profileScreen.item2,
         navigationBarItems.aboutScreen.item2
       ]);
     });
