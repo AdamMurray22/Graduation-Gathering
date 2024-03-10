08ed50ed3b7f06bb5985a72e20dd755822a3d200
Adam-Murray <adammurray1122@gmail.com>
2024-03-10 01:06:05 +0000

    Added drop downs for faculty,school,course on profile screen

--- Graduation-Gathering/graduation_gathering/lib/Screen/profile_screen.dart
+++ Graduation-Gathering/graduation_gathering/lib/Screen/profile_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_gathering/Profile/set_user_profile.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
 
 import '../Auth/auth_token.dart';
-import '../Map/main_map_widget.dart';
 import '../Profile/profile_settings.dart';
 
 /// This holds the screen for the application.
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key, required this.authToken, required this.profile});
+  const ProfileScreen(
+      {super.key, required this.authToken, required this.profile});
 
   final AuthToken authToken;
   final ProfileSettings profile;
@@ -19,16 +20,37 @@ class ProfileScreen extends StatefulWidget {
 // This class contains the GUI structure for the app.
 class _ProfileScreenState extends State<ProfileScreen> {
   late SetUserProfile _setUserProfile;
+  late SingleValueDropDownController _facultyDropDownController;
+  late SingleValueDropDownController _schoolDropDownController;
+  late SingleValueDropDownController _courseDropDownController;
+
+  late List<DropDownValueModel> _facultyDropDownList = [];
+  late List<DropDownValueModel> _schoolDropDownList = [];
+  late List<DropDownValueModel> _courseDropDownList = [];
+
+  bool _schoolVisible = false;
+  bool _courseVisible = false;
 
   String _email = "";
   String _accountType = "";
   String? _name;
+  String? _faculty;
+  String? _school;
+  String? _course;
 
-  @override void initState() {
+  @override
+  void initState() {
     _setUserProfile = SetUserProfile(widget.authToken);
     _email = widget.profile.getEmail();
     _accountType = widget.profile.getAccountType();
     _name = widget.profile.getName();
+    _faculty = widget.profile.getFaculty();
+    _school = widget.profile.getSchool();
+    _course = widget.profile.getCourse();
+
+    _facultyDropDownController = SingleValueDropDownController();
+    _schoolDropDownController = SingleValueDropDownController();
+    _courseDropDownController = SingleValueDropDownController();
     super.initState();
   }
 
@@ -41,10 +63,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
-          child: Padding(
-        
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: Column(
+            child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -56,16 +77,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
               Text("Account Type: $_accountType",
                   style: const TextStyle(fontSize: 22)),
               const SizedBox(height: 5),
-              Row(
-                  children: [
-                    const Text("Name: ", style: TextStyle(fontSize: 22)),
-                    const SizedBox(width: 3),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.75,
-                        child:TextField(
+              Row(children: [
+                const Text("Name: ", style: TextStyle(fontSize: 22)),
+                const SizedBox(width: 3),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: TextField(
                       onChanged: (text) {
-                        if (text.length > 50)
-                        {
+                        if (text.length > 50) {
                           text = text.substring(0, 50);
                         }
                         _name = text;
@@ -74,22 +93,127 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(
                           borderSide:
-                          BorderSide(color: Colors.black, width: 0.0),
+                              BorderSide(color: Colors.black, width: 0.0),
                         ),
                         hintText: 'Enter your name here...',
                       ),
                     )),
-                  ]),
+              ]),
               const SizedBox(height: 5),
-              const Text("Faculty:", style: TextStyle(fontSize: 22)),
+              Row(children: [
+                const Text("Faculty:", style: TextStyle(fontSize: 22)),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: DropDownTextField(
+                      key: const Key("Faculty Search box"),
+                      controller: _facultyDropDownController,
+                      initialValue: _faculty,
+                      clearOption: true,
+                      enableSearch: true,
+                      textFieldDecoration:
+                          const InputDecoration(hintText: "Faculty"),
+                      searchDecoration: const InputDecoration(
+                          hintText: "Enter your faculty here"),
+                      validator: (value) {
+                        if (value == null) {
+                          return "Required field";
+                        } else {
+                          return null;
+                        }
+                      },
+                      dropDownItemCount: 5,
+                      dropDownList: _facultyDropDownList,
+                      onChanged: (value) {
+                        if (value == "") {
+                          _schoolVisible = false;
+                          value = null;
+                        } else {
+                          _schoolVisible = true;
+                        }
+                        _faculty = value;
+                        setState(() {});
+                      },
+                    ))
+              ]),
               const SizedBox(height: 5),
-              const Text("School:", style: TextStyle(fontSize: 22)),
+              Visibility(
+                  visible: _schoolVisible,
+                  child: Row(children: [
+                    const Text("School:", style: TextStyle(fontSize: 22)),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        child: DropDownTextField(
+                          key: const Key("School Search box"),
+                          controller: _schoolDropDownController,
+                          initialValue: _school,
+                          clearOption: true,
+                          enableSearch: true,
+                          textFieldDecoration:
+                              const InputDecoration(hintText: "School"),
+                          searchDecoration: const InputDecoration(
+                              hintText: "Enter your school here"),
+                          validator: (value) {
+                            if (value == null) {
+                              return "Required field";
+                            } else {
+                              return null;
+                            }
+                          },
+                          dropDownItemCount: 5,
+                          dropDownList: _schoolDropDownList,
+                          onChanged: (value) {
+                            if (value == "") {
+                              _courseVisible = false;
+                              value = null;
+                            } else {
+                              _courseVisible = true;
+                            }
+                            _school = value;
+                            setState(() {});
+                          },
+                        ))
+                  ])),
               const SizedBox(height: 5),
-              const Text("Course: ", style: TextStyle(fontSize: 22)),
+              Visibility(
+                  visible: _courseVisible,
+                  child: Row(children: [
+                    const Text("Course: ", style: TextStyle(fontSize: 22)),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        child: DropDownTextField(
+                          key: const Key("Course Search box"),
+                          controller: _courseDropDownController,
+                          initialValue: _course,
+                          clearOption: true,
+                          enableSearch: true,
+                          textFieldDecoration:
+                              const InputDecoration(hintText: "Course"),
+                          searchDecoration: const InputDecoration(
+                              hintText: "Enter your course here"),
+                          validator: (value) {
+                            if (value == null) {
+                              return "Required field";
+                            } else {
+                              return null;
+                            }
+                          },
+                          dropDownItemCount: 5,
+                          dropDownList: _courseDropDownList,
+                          onChanged: (value) {
+                            if (value == "") {
+                              value = null;
+                            }
+                            _course = value;
+                            setState(() {});
+                          },
+                        ))
+                  ])),
               Container(
                 alignment: Alignment.bottomRight,
                 child: ElevatedButton(
-                  onPressed: () {_saveSettings();},
+                  onPressed: () {
+                    _saveSettings();
+                  },
                   child: const Text("Save", style: TextStyle(fontSize: 17)),
                 ),
               ),
@@ -98,13 +222,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
         )));
   }
 
-  _saveSettings()
-  {
+  _saveSettings() {
     widget.profile.setHasLoggedInBefore(true);
     widget.profile.setName(_name);
-    //widget.profile.setFaculty(faculty);
-    //widget.profile.setSchool(school);
-    //widget.profile.setCourse(course);
+    widget.profile.setFaculty(_faculty);
+    widget.profile.setSchool(_school);
+    widget.profile.setCourse(_course);
     _setUserProfile.send(widget.profile);
   }
 }
